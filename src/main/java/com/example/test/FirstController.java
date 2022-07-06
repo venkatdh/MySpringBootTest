a851048160a88acd01e20a941f4f126c7ee35756
Venkat <Venkat@DESKTOP-QES1DO1>
2022-07-06 16:38:00 +0530

    Commited first time

---
+++ src/main/java/com/example/test/FirstController.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstController {
+
+	public void message() {
+	System.out.println("Welcome to git hub");
+	}
+}
